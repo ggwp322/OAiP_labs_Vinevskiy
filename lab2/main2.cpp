1c1079cfd3a7bbf2571dce05b83a729c1d5ec0e1
Maxim <31659159+ggwp322@users.noreply.github.com>
2017-10-30 18:59:52 +0300

    Add files via upload

---
+++ lab2/main2.cpp
@@ -0,0 +1,9 @@
+/* Виневский Максим Викторович
+Лабораторная работа №1
+Вариант №5
+Задание :Перевести лот в карат и выразить в унциях.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<locale.h>
+#include<conio.h>
