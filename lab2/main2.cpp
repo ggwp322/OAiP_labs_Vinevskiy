81544f097174e601678c498ee25b5569fe0a9c79
Maxim <31659159+ggwp322@users.noreply.github.com>
2017-10-30 19:00:23 +0300

    Delete main2.cpp

--- lab2/main2.cpp
+++
@@ -1,9 +0,0 @@
-/* Виневский Максим Викторович
-Лабораторная работа №1
-Вариант №5
-Задание :Перевести лот в карат и выразить в унциях.
-*/
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<locale.h>
-#include<conio.h>
